6965c23b82876ec96a4f07e83994b7ec9265762b
nitin <nitin@NITIN>
2026-08-13 15:40:51 +0530

    Merge branch 'TestScript3_Smoke' of https://github.com/Nitinnaik1998/IBM_Swiggy_SWG1.0.git into TestScript3_Smoke

--- Swiggy_SWG1.0/src/main/java/com/swiggy/ObjectRepo/SearchDetails_Nykaa.java
+++ Swiggy_SWG1.0/src/main/java/com/swiggy/ObjectRepo/SearchDetails_Nykaa.java
@@ -21,7 +21,7 @@ public class SearchDetails_Nykaa {
 		return productsCount;
 	}
 
-	@FindBy(xpath = "(//button[@aria-label='View similar products'])[1]")
+	@FindBy(xpath= "(//div[@data-at='product-title'])[1]")
 	private WebElement FirstProduct;
     
 	
